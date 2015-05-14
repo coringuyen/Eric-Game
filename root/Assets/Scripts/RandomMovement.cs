1efa7fffbb624e19cfb790f00d98258fbbcec4de
coringuyen <tran.nguyen@students.aie.edu.au>
2015-05-14 17:11:02 +1100

    enemy move
    added enemy movement

---
+++ root/Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+    
+
+
+public class RandomMovement : MonoBehaviour {
+
+
+	void Update () 
+    {
+        // move forward by z axis
+        transform.Translate(Vector3.forward * Time.deltaTime);
+        // rotate randomly by y axis
+        transform.Rotate(new Vector3(0.0f, Random.Range(-200, 200), 0.0f) * Time.deltaTime);
+	}
+}
